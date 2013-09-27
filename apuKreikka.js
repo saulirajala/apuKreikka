a2b673a388e51a9e01ea09917e6fc9ef32af221f
Sauli Rajala <sajoraja@gmail.com>
2013-09-27 20:49:08 +0300

    Pientä säätöä

--- apuKreikka.js
+++ apuKreikka.js
@@ -3,5 +3,45 @@
  * and open the template in the editor.
  */
 window.onload = function() {
-    alert ("Moi");
+    //luodaan screen-luokan olio
+    var naytto = new Screen("Roomalaiskirje");
+    document.write(naytto.kirje);
+    naytto.tulostaMain();
 }
+
+
+/**
+ * Screen olio, joka huolehtii ohjelman tulostamisen
+ * Huolehtii lomakkeen ja muun näkyvän tulostamisesta sekä isännöi globaaleja 
+ * metodeja. Olion vastuulla kaikki tulostettava ja käyttäjän syöte.
+ *
+ * Attribuutit:
+ * -kirje???
+ * Metodit:
+ * tulostaLomake()
+ * tulostaLyhenteet()
+ * tulostaMenu()???
+ * tarkista(input, olio.getSijamuoto())
+ * merkkaa(input color, olio.setColor())
+ * @returns {Screen}
+ */
+function Screen(kirje)
+{
+    // Sijoitetaan parametreina tulevat tiedot olion omiin muuttujiin:
+    this.kirje = kirje;
+
+    // Liitetään metodi olioon:
+    this.tulostaMain = tulostaMain;
+
+
+    /*
+     * funktion tehtävänä on tulostaa käyttöliittymän pääikkuna
+     */
+    function tulostaMain()
+    {
+        document.write('<div class="jae"><h2>1:</h2>');
+        document.write('<div class="solu">');
+        document.write('<input /><label>Greek</label><input />');
+        document.write('</div></div>');
+    }
+} 
--- index.php
+++ index.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html>
+<?php echo '<?xml version = "1.0" encoding = "utf-8" ?>' ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fi" >
     <head>
+        <link href="apuKreikka.css" rel="StyleSheet" type="text/css" />
+        <script language="JavaScript" src="apuKreikka.js" type="text/javascript"></script>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+            <title>apuKreikka 2.0</title>
     </head>
     <body>
         <h1>Uusi ja uljas apuKreikka 2.0. Tällä kertaa olioilla. </h2>
-        <?php
-        // put your code here
-        ?>
+            <?php
+            // put your code here
+            ?>
     </body>
 </html>
