b92f5c4361ca88db6a4058bd0f055247b3c11877
Sauli Rajala <sajoraja@gmail.com>
2013-09-27 19:07:52 +0300

    indeksi sivu

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
-<?php
-echo "JEEJEEJEJEJEJEJ";
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Uusi ja uljas apuKreikka 2.0</h2>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
